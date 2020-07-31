95297de1d66c413bed676045cf2b65d8743cface
Travis Fox <travis.fox90@gmail.com>
2020-07-31 01:01:52 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,15 @@
-# Write your code here
+require "open-uri"
+require "net/http"
+
+
+ GetRequester
+  
+  def initialize(url)
+    @url = url
+  end
+  
+  def get_response_body
+    
+  end
+  
+end
